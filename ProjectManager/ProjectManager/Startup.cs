e2d6d7f48aabe96e8a826d4c510896d7c9b07e30
Spencer Ritzow <sritzow_h@dev.bse.edu>
2015-06-15 17:02:35 -0500

    Initial Commit of MVC Project.

---
+++ ProjectManager/ProjectManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectManager.Startup))]
+namespace ProjectManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
