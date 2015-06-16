1f12aca3ea973e8c9e732270b6b13123ee73f5aa
Spencer Ritzow <sritzow_h@dev.bse.edu>
2015-06-16 16:21:47 -0500

    Added Classes for possible Database entries.
    Project-
    Relates to project table
    
    Project Auth-
    Relates to who is authorized to view the project
    
    Calendar-
    Relates to the calendar database
    
    Milestone-
    Relates to a time period in which tasks need to be done in.
    
    Task-
    Relates to a task of work that needs to be done in x hours
    
    TaskUser-
    Who is assigned to the task
    
    Event-
    Relates to an event happing

---
+++ ProjectManager/ProjectManager/Models/ProjectViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectManager.Models
+{
+    public class ProjectViewModel
+    {
+
+    }
+
+    public class Project
+    {
+        public int id { get; set; }
+        public int name { get; set; }
+        public int description { get; set; }
+    }
+
+    public class ProjectAuth
+    {
+        public int projectId { get; set; }
+        public int userId { get; set; }
+        public int permessions { get; set; }
+    }
+
+    public class Calendar
+    {
+        public int id { get; set; }
+        public int projectId { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Milestone
+    {
+        public int id { get; set; }
+        public int calendarId { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+    }
+
+    public class Task
+    {
+        public int id { get; set; }
+        public int milestoneId { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public int hours { get; set; }
+    }
+
+    public class TaskUser
+    {
+        public int id { get; set; }
+        public int taskId { get; set; }
+        public int userId { get; set; }
+    }
+
+    public class Event
+    {
+        public int id { get; set; }
+        public int projectId { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public long time { get; set; }
+    }
+}
